78369d8fe72293bb98e66c1f0066c4f77cd4eee9
2425343005 <rlawldbs0628@naver.com>
2024-10-16 19:16:48 +0900

    1.TripMapper(interface, xml) 작성 2.TripDetailDTO 작성-> pk로 들어가는 객체 작성해야함

--- WithTrip/src/main/java/com/withtrip/domain/TripPlanDTO.java
+++ WithTrip/src/main/java/com/withtrip/domain/TripPlanDTO.java
@@ -1,5 +1,21 @@
 package com.withtrip.domain;
 
+import java.time.LocalDate;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class TripPlanDTO {
+	
+	private Long plan_id;
+	private String user_email;
+	private String user_name;
+	private String user_pwd;
+	private LocalDate start_date;
+	private LocalDate end_date;
+	private LocalDate plan_created;
+	private LocalDate plan_update;
 
 }
--- WithTrip/src/main/java/com/withtrip/mapper/TripPlanMapper.java
+++ WithTrip/src/main/java/com/withtrip/mapper/TripPlanMapper.java
@@ -1,5 +1,33 @@
 package com.withtrip.mapper;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.withtrip.domain.TripDetailDTO;
+import com.withtrip.domain.TripPlanDTO;
+
+@Mapper
 public interface TripPlanMapper {
+	
+	// 여행계획 저장
+	int insertTripPlan(TripPlanDTO tripPlan);
+	
+	// 세부계획 저장
+	int insertTripDetail(TripDetailDTO tripDetail);
+	
+	// 모든 여행계획 조회
+	List<TripPlanDTO> getAllTripPlans();
+	
+	// 여행계획 조회
+	TripPlanDTO getTripPlan(@Param("plan_id") long plan_id);
+	
+	// 세부계획 조회
+	List<TripDetailDTO> getTripDetail(@Param("plan_id") long plan_id);
+	
+	// 단일 세부계획 조회
+	TripDetailDTO getDetailById(@Param("derail_id") long detail_id);
+	
 
 }
